62e28b29d5713d16106b5dfb57fdf65bc2649a2f
MoglinV2 <100311079+Moglinv2@users.noreply.github.com>
2023-05-12 01:34:52 -0400

    Update TermsOfService.vue

--- resources/js/Pages/TermsOfService.vue
+++ resources/js/Pages/TermsOfService.vue
@@ -11,7 +11,7 @@ defineProps({
     <Head title="Terms of Service" />
 
     <div class="font-sans text-gray-900 antialiased">
-        <div class="pt-4 bg-gray-100">
+        <div class="pt-4 bg-gray-900">
             <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
                 <div>
                     <JetAuthenticationCardLogo />
